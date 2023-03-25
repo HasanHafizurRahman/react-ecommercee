be8a8f98f601f566fe4442659da91650c2e11f6e
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-03-25 22:22:27 +0600

    cart function

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,7 +1,15 @@
 import React, { useEffect, useState } from "react";
+import Product from "./Product";
 
 const Shop = () => {
   const [product, setProduct] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  const handleAddToCart = (product) => {
+    // const newCart = [...cart, product]
+    setCart([...cart, product]);
+  };
+
   useEffect(() => {
     fetch("products.json")
       .then((res) => res.json())
@@ -10,19 +18,18 @@ const Shop = () => {
   return (
     <div className="flex">
       <div className="w-4/5 grid grid-cols-1 lg:grid-cols-3 px-10">
-        {product.map((item) => (
-          <div className="w-[286px] border 1px #95A0A7 rounded-md">
-            <img className="w-[286px] h-[286px]" alt="shoe" src={item.img} />
-            <h2 className="text-lg pl-2">{item.name}</h2>
-            <h5 className="text-sm pl-2">Price:${item.price}</h5>
-            <br />
-            <p className="text-xs pl-2">{item.seller}</p>
-            <p className="text-xs pl-2">Ratings: {item.ratings} star</p>
-            <button className="bg-[#FFE0B3] w-full mt-3">Add to cart</button>
-          </div>
+        {product.map((product) => (
+          <Product
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          />
         ))}
       </div>
-      <div className="w-1/5 h-[944px] bg-[#FF9900] mr-11">Shop Cart</div>
+      <div className="w-1/5 h-[944px] bg-[#FF9900] mr-11">
+        <h1>Shop Cart</h1>
+        <h2>Selected Products :{cart.length} </h2>
+      </div>
     </div>
   );
 };
